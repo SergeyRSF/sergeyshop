b80de5714e3bc042962c3b371b3694345fbc415f
SergeyRSF <seregich2195@gmail.com>
2017-03-21 23:15:43 +0300

    Настройка галпа

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var notify = require("gulp-notify");
+var sourcemaps = require('gulp-sourcemaps');
+var cleanCSS = require('gulp-clean-css');
+var browserSync = require('browser-sync').create();
+
+
+
+gulp.task('server', ['sass'] , function(){
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+    gulp.watch('./scss/**/*.scss', ['sass']);
+    gulp.watch('./*.html').on('change', browserSync.reload);
+})
+
+gulp.task('sass', function(){
+    gulp.src('./scss/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass({outputStyle: 'compressed'}) .on('error', sass.logError))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./css'))
+    .pipe(notify("Compiled file: <%= file.relative %>!"))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('default', ['server']);
